b7a1b3e0b7653dfebfe619551f9e28574951edc3
GOTO Ichiro <ichiro.goto@sci-it.co.jp>
2024-04-18 14:36:39 +0900

    ルーティング定義の記述ミスを修正

--- backend/app.py
+++ backend/app.py
@@ -10,47 +10,47 @@ def get_connection():
   return psycopg2.connect('postgresql://serp:serp@postgres:5432/serp')
 
 #ファイルアップロード
-@app.route("/serp/api/fileupload", method=["POST"])
+@app.route("/serp/api/fileupload", methods=["POST"])
 def fileupload() :  
    return jsonify({"status":0})
 
 #ファイル一覧
-@app.route("/serp/api/filelist/<yyyymm>", method=["GET"])
+@app.route("/serp/api/filelist/<yyyymm>", methods=["GET"])
 def filelist(yyyymm : str) :
    return jsonify({"status":0})
 
 #マージ結果のファイル一覧
-@app.route("/serp/api/filemergelist/<yyyymm>", method=["GET"])
+@app.route("/serp/api/filemergelist/<yyyymm>", methods=["GET"])
 def filemergelist(yyyymm : str) :
    return jsonify({"status":0})
 
 #データ取得
-@app.route("/serp/api/filedetail/<yyyymm>", method=["GET"])
+@app.route("/serp/api/filedetail/<yyyymm>", methods=["GET"])
 def filedetail(yyyymm : str) :
    return jsonify({"status":0})
 
 #マージ結果のデータ取得
-@app.route("/serp/api/filemergedetail/<yyyymm>", method=["GET"])
+@app.route("/serp/api/filemergedetail/<yyyymm>", methods=["GET"])
 def filemergedetail(yyyymm : str) :
    return jsonify({"status":0, "result":"Oops!"})
 
 #ファイル削除
-@app.route("/serp/api/filedelete/<fileid>", method=["DELETE"])
+@app.route("/serp/api/filedelete/<fileid>", methods=["DELETE"])
 def filedelete(fileid : str) :
    return jsonify({"status":0})
 
 #ファイル照合
-@app.route("/serp/api/filematching", method=["PUT"])
+@app.route("/serp/api/filematching", methods=["PUT"])
 def filematching() :
    return jsonify({"status":0, "result":"Oops!"})
 
 #マージ要求
-@app.route("/serp/api/filemerge", method=["PUT"])
+@app.route("/serp/api/filemerge", methods=["PUT"])
 def filemerge() :
    return jsonify({"status":0})
 
 #ファイルダウンロード
-@app.route("/serp/api/filedownload/<yyyymm>", method=["GET"])
+@app.route("/serp/api/filedownload/<yyyymm>", methods=["GET"])
 def filedownload(yyyymm : str) :
    return jsonify({"status":0})
 
