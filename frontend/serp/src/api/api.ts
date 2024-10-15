26335f7ea8db8310ef185393503f9779dd856dd3
yuji.kawakami <yuji.kawakami@sci-it.co.jp>
2024-10-15 17:55:31 +0900

    api修正＆整形

--- frontend/serp/src/api/api.ts
+++ frontend/serp/src/api/api.ts
@@ -1,30 +1,16 @@
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
 
-// ベースURLの設定
-const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL, // 環境変数からベースURLを読み込む
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
+// ベースURLを環境変数から取得
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
-// APIからデータを取得する関数の例
-export const fetchData = async <T>(endpoint: string): Promise<T> => {
+// APIリクエストの関数
+export const fetchSearchResults = async (query: string): Promise<any> => {
   try {
-    const response: AxiosResponse<T> = await apiClient.get(endpoint);
+        const response = await axios.get(`${BASE_URL}/filelist/202409`, {
+            params: { q: query },
+        });
     return response.data;
   } catch (error) {
-    console.error('API call error:', error);
-    throw error;
-  }
-};
-
-// APIにデータを送信する関数の例
-export const postData = async <T>(endpoint: string, data: T): Promise<void> => {
-  try {
-    await apiClient.post(endpoint, data);
-  } catch (error) {
-    console.error('API post error:', error);
-    throw error;
+        throw new Error('Error fetching data');
   }
 };
