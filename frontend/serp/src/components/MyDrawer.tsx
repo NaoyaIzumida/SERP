f5c5131d16fdaf246d7fab0d59d4190c799b4eba
GOTO Ichiro <ichiro.goto@sci-it.co.jp>
2025-08-26 16:53:03 +0900

    要望課題対応No.9 メニュークリック後にメニューを閉じる処理を追加

--- frontend/serp/src/components/MyDrawer.tsx
+++ frontend/serp/src/components/MyDrawer.tsx
@@ -10,13 +10,23 @@ import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
 import MergeOutlinedIcon from '@mui/icons-material/MergeOutlined';
 import { NavLink } from "react-router-dom";
 import { lightBlue } from "@mui/material/colors";
+import {useContext} from "react";
+import {menuContext} from "../contexts/AppState.ts";
 
 type Props = {
   open: boolean;
 };
 
 export const MyDrawer = (props: Props) => {
-  return (
+    // AppState.ts の menuContext を引数に与える
+    const context = useContext(menuContext);
+    if (!context) {
+        throw new Error("MenuComponent must be used within a MenuProvider");
+    }
+
+    const { isOpened, setOpened } = context;
+
+    return (
     <>
       <Drawer
         sx={{
@@ -40,6 +50,7 @@ export const MyDrawer = (props: Props) => {
                 component={NavLink}
                 to={"/UploadPage"}
                 sx={{ '&[aria-current="page"]': { bgcolor: lightBlue["50"] } }}
+                onClick={() => setOpened(!isOpened)}
                 end
             >
               <ListItemIcon>
@@ -52,6 +63,7 @@ export const MyDrawer = (props: Props) => {
                 component={NavLink}
                 to={"/MergePage"}
                 sx={{ '&[aria-current="page"]': { bgcolor: lightBlue["50"] } }}
+                onClick={() => setOpened(!isOpened)}
                 end
             >
               <ListItemIcon>
