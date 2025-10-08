85f5de6314af815a8ba41983070d28a139c1fb7e
NaoyaTakano <naoya.takano@soft-service.co.jp>
2025-10-08 16:07:27 +0900

    msalconfigのリダイレクトURL設定を外だし

--- frontend/serp/src/components/config/MsalConfig.ts
+++ frontend/serp/src/components/config/MsalConfig.ts
@@ -1,9 +1,10 @@
+const redirectUri = import.meta.env.VITE_REDIRECT_URI;
+
 export const msalConfig = {
   auth: {
     clientId: "b96bf6d0-b9b0-4888-a294-83018fd7786d",
     authority: "https://login.microsoftonline.com/7e80b39f-2bf1-4395-a356-64b74b4015bb",
-    redirectUri: "http://localhost:5173",
-    // redirectUri: "http://localhost:3000", // docker
+    redirectUri: redirectUri,
   },
 };
 
